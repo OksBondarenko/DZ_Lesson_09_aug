4c69d59b56eeb7b7f57a646274641d241b8c0aae
Oksana bondarenko <roksanapopova@gmail.com>
2022-08-13 17:34:05 +0200

    Task 10 + gitignore

---
+++ Задача10/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 10
+//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+int SecondDigit(int number)
+{
+    int secDigit;
+    number = Math.Abs(number);
+    if (number > 99 && number < 1000)
+    {
+        secDigit = number / 10 % 10;
+        return secDigit;
+    }
+    else
+    {
+        secDigit = -1;
+        return secDigit;
+    }
+}
+
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (SecondDigit(num)> -1)
+    Console.WriteLine(SecondDigit(num));
+else
+    Console.WriteLine("Введено не трехзначное число!");
